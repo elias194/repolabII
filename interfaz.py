523601797e90f0d378ee3708d91db3df91dc9a4e
Pablo Boccardo <pabloboccardopabloboc@gmail.com>
2024-06-04 07:28:27 -0300

    Update interfaz.py
    nicee

--- interfaz.py
+++ interfaz.py
@@ -3,6 +3,8 @@ from customtkinter import *
 root = CTk()
 root.geometry("1024x768")
 root.config(background="#000000")
-set_appearance_mode("light")
+set_appearance_mode("dark")
+
+label = CTkLabel(master=root)
 
 root.mainloop()
