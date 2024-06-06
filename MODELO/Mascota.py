404a646b4620f303822b9626e874bafe21f7d888
JoiBG <pabloboccardopabloboc@gmail.com>
2024-06-06 19:04:23 -0300

    Reapply "Merge branch 'main' into Pabloo"
    This reverts commit 02029786a31e9167f8dd9043062f88fde8db206c.

---
+++ MODELO/Mascota.py
@@ -0,0 +1,17 @@
+class Mascota:
+    def __init__(self,estado="",nombre="",raza="",fecha_nacimiento="",propietario="",fichamedica=""):
+        self.estado = estado
+        self.nombre = nombre
+        self.raza = raza
+        self.fecha_nacimiento = fecha_nacimiento
+        self.propietario = propietario
+        self.fichamedica=fichamedica
+
+    def getEstado(self):
+        return self.estado
+    def setEstado(self):
+        return self.estado
+    def __str__(self):
+        return self.estado, self.nombre, self.raza, self.fecha_nacimiento, self.propietario,self.codigo_propietario
+    def __repr__(self):
+        return self.estado, self.nombre, self.raza, self.fecha_nacimiento, self.propietario,self.codigo_propietario
