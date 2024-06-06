38abc8f6f811feb2a0bfe57debbe171dcc94bb2b
JoiBG <pabloboccardopabloboc@gmail.com>
2024-06-06 18:01:56 -0300

    Traspaso de clases

--- interfaz.py
+++ interfaz.py
@@ -1,3 +1,4 @@
+"""
 from customtkinter import *
 
 root = CTk()
@@ -7,4 +8,23 @@ set_appearance_mode("dark")
 
 label = CTkLabel(master=root)
 
-root.mainloop()
+
+
+root.mainloop() """
+
+import customtkinter as tk
+
+class MyApp(tk.Application):
+    def create_widgets(self):
+        self.button = tk.Button(self, text="Abrir ventana", command=self.open_window)
+        self.button.pack()
+
+    def open_window(self):
+        second_window = tk.Toplevel(self)
+        second_window.title("Segunda ventana")
+        label = tk.Label(second_window, text="Esta es otra ventana")
+        label.pack()
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.mainloop()
