57af4cf302150932ccd661031b77fe8fe1c3dc76
JoiBG <pabloboccardopabloboc@gmail.com>
2024-06-06 18:20:12 -0300

    borre

--- MODELO/Propietario.py
+++
@@ -1,8 +0,0 @@
-class Veterinario(Persona):
-    def __init__(self,nombre,direccion,mail,telefono,nummatricula,especializacion):
-        super().__init__(nombre,direccion,mail,telefono)
-        self.nummatricula = nummatricula
-        self.especializacion = especializacion
-
-    def mostrarveterinario(self):
-        return "veterinario:",self.nombre,"direccion:",self.direccion,"mail:",self.mail,"telefono:",self.telefono
